c0b0b5387f0ddac033ef9e9935025265c6fe09ec
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-07-12 10:48:19 +0530

    created project structure and install env package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`port listening on ${PORT}`));
