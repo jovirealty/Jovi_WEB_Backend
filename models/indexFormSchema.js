227b85555d59de43d6699d6a45163201c6ba0c8b
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-07-14 09:31:28 +0530

    home page schema

---
+++ models/indexFormSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const contactDetailSchema = new mongoose.Schema({
+    countryCode: {
+        type: String, 
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+    }
+}, {_id: false});
+
+const indexFormSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+    },
+    contactDetail: {
+        type: contactDetailSchema,
+        required: true,
+    },
+    iAm: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('indexForm', indexFormSchema);
