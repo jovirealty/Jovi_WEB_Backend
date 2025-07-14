7747452d1ae31418febea7c39e1c8f6f0988962d
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-07-14 08:42:55 +0530

    updated routes and install cors packages

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+connectDB();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/homepage', require('./routes/indexForm'))
+
+// error Handler
+app.use(errorHandler);
 
 const PORT =  process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`port listening on ${PORT}`));
