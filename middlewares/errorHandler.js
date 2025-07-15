d1e250d12de7832ac9de83f1c307407c0dc2d447
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-07-15 09:44:39 +0530

    removed extra log

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    console.error('Error:', err);
-    console.log(err.stack);
+    console.log("error: ", err.stack);
     res.status(500).json({success: false, message: err.message, error: err});
 };
 
