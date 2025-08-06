1aafe7ace7a07ce3bee99b81593b83838ea2787a
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-08-06 10:20:50 +0530

    Added - searchAgent for search and paginations on /agents route

--- routes/agentListRoutes.js
+++ routes/agentListRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const AgentController = require('../controllers/AgentListController');
-const verifyCaptcha = require('../middlewares/verifyCaptcha');
+// const verifyCaptcha = require('../middlewares/verifyCaptcha');
 
 // Webhook routes
 router.post('/agents/sync', AgentController.upsertAgents);
 
 // GET agent Routes
-router.get('/agents', AgentController.getAgents);
+router.get('/agents', AgentController.searchAgents)
+// router.get('/agents', AgentController.getAgents);
 router.get('/agents/:id', AgentController.getAgentById);
 
 module.exports = router;
