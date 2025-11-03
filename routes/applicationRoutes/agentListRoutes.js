f4e3724f3e65882805502e684349c7212db13d76
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-11-03 23:53:14 +0530

    Updated - routes in agentlistRoute.js for with agentProperty GET route

--- routes/applicationRoutes/agentListRoutes.js
+++ routes/applicationRoutes/agentListRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const AgentController = require('../../controllers/AgentListController');
+const { getAllProperty } = require('../../controllers/StaffAddPropertyControllers/webapp/AgentPropertyListingsController');
 // const verifyCaptcha = require('../middlewares/verifyCaptcha');
 
 // Webhook routes
 router.post('/agents/sync', AgentController.upsertAgents);
+
+// Off-Market listings (agentProperties)
+router.get('/agents/propertylistings', getAllProperty);
+// router.get('/agent/propertylistings/:id', getPropertyById);
 
 // GET agent Routes
 router.get('/agents', AgentController.searchAgents)
